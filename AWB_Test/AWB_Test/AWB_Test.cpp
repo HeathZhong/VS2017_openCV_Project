0895997ca46f43404722e15ac4ee4e19a153eafb
HeathZhong <zhongqiang0812@thundersoft.com>
2019-09-18 10:24:55 +0800

    original version

---
+++ AWB_Test/AWB_Test/AWB_Test.cpp
@@ -0,0 +1,49 @@
+﻿// AWB_Test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat InputImage, OutImage, ContrastImage;
+	InputImage = imread("scene.jpg", IMREAD_COLOR);
+	if (InputImage.data == nullptr)
+	{
+		cout << "Image load error!" << endl;
+		return 0;
+	}
+	imshow("AWB_前", InputImage);
+
+	vector<Mat> ImageBGR;
+	split(InputImage, ImageBGR);
+	double B_mean, G_mean, R_mean;
+	B_mean = mean(ImageBGR[0], noArray())[0];
+	G_mean = mean(ImageBGR[1], noArray())[0];
+	R_mean = mean(ImageBGR[2], noArray())[0];
+	double B_gain, G_gain, R_gain;
+	B_gain = (B_mean + G_mean + R_mean) / (3 * B_mean);
+	G_gain = (B_mean + G_mean + R_mean) / (3 * G_mean);
+	R_gain = (B_mean + G_mean + R_mean) / (3 * R_mean);
+	ImageBGR[0] = ImageBGR[0] * B_gain;
+	ImageBGR[1] = ImageBGR[1] * G_gain;
+	ImageBGR[2] = ImageBGR[2] * R_gain;
+	merge(ImageBGR, OutImage);
+	imshow("AWB_后", OutImage);
+	ContrastImage = imread("scene1.jpg", IMREAD_COLOR);
+	imshow("AWB_手机", ContrastImage);
+
+	while (true)
+	{
+		if (waitKey(10) == 27)
+			break;
+	}
+	
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
---
+++ Connect_Domain/Connect_Domain/Connect_Domain.cpp
@@ -0,0 +1,41 @@
+﻿// Connect_Domain.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat InputImage, OutImage;
+	InputImage = imread("cell.jpg", IMREAD_COLOR);
+	if (InputImage.data == nullptr)
+	{
+		cout << "Load image error !" << endl;
+		return 0;
+	}
+	imshow("src", InputImage);
+	Mat BinaryImage, GrayImage, CannyImage;
+	cvtColor(InputImage, GrayImage, COLOR_BGR2GRAY);
+	imshow("Gray", GrayImage);
+	
+	Canny(GrayImage, CannyImage, 30, 60);
+// 	erode(CannyImage, CannyImage, 3);  //腐蚀
+// 	dilate(CannyImage, CannyImage, 3); //膨胀
+	imshow("Canny",CannyImage);
+	findContours(CannyImage, );
+	//imshow("Binary", BinaryImage);
+
+	while (true)
+	{
+		if (waitKey(10) == 27)
+			break;
+	}
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
